fc510ebb3ee147814f85fb0f5fc3eb100ccc0542
kmarie2001 <143460313+kmarie2001@users.noreply.github.com>
2023-11-05 22:17:18 -0700

    Attempt 2
    added this._____ to returns in all classes

--- src/Cat.java
+++ src/Cat.java
@@ -12,16 +12,16 @@ public class Cat implements AdoptableAnimal{
 
     @Override
     public String getName(){
-        return name;
+        return this.name;
     }
 
     @Override
     public String getDescription(){
-        return description;
+        return this.description;
     }
 
     @Override
     public double getPrice(){
-        return price;
+        return this.price;
     }
 }
--- src/Dog.java
+++ src/Dog.java
@@ -11,3 +11,8 @@ public abstract class Dog implements AdoptableAnimal{
     }
     public abstract String getBreed();
 }
+//Side note: you can implement the getters
+// for the dog fields inside the dog class
+// and only override the breed name in the
+// dog subclasses. Not require, but a good
+// simplification that you can be aware of.
